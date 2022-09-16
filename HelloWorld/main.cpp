53f55ded8555fde0369f012a449101446653b39c
DANIEL KAPRANOV <ac2266258@A312-14.local>
2022-09-16 13:30:07 +0500

    Hello Mukhammadali Khayotov

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by DANIEL KAPRANOV on 16/09/22.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    cout << "Hello, World!" << endl;
+    
+    return 0;
+}
